e5abcb9a759593a3a5f781fcc731f3ee1ed398bf
Eman Basic <basic.eman@hotmail.com>
2022-10-14 12:43:33 +0200

    rename the project from 'Charts' to 'DGCharts' for compatibility with native Swift Charts

--- ChartsDemo-iOS/Swift/Demos/CombinedChartViewController.swift
+++ ChartsDemo-iOS/Swift/Demos/CombinedChartViewController.swift
@@ -9,7 +9,7 @@
 #if canImport(UIKit)
     import UIKit
 #endif
-import Charts
+import DGCharts
 #if canImport(UIKit)
     import UIKit
 #endif
@@ -45,11 +45,11 @@ class CombinedChartViewController: DemoBaseViewController {
         chartView.highlightFullBarEnabled = false
         
         
-        chartView.drawOrder = [DrawOrder.bar.rawValue,
-                               DrawOrder.bubble.rawValue,
-                               DrawOrder.candle.rawValue,
-                               DrawOrder.line.rawValue,
-                               DrawOrder.scatter.rawValue]
+//        chartView.drawOrder = [DrawOrder.bar.rawValue,
+//                               DrawOrder.bubble.rawValue,
+//                               DrawOrder.candle.rawValue,
+//                               DrawOrder.line.rawValue,
+//                               DrawOrder.scatter.rawValue]
         
         let l = chartView.legend
         l.wordWrapEnabled = true
